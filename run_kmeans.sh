78ab2e578d719786b80e7444931de5d72318b08e
Junhwan Kim <junhwank@umich.edu>
2020-04-27 06:39:10 -0400

    fixed kmeans, ran on diff k

--- run_kmeans.sh
+++ run_kmeans.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+cd kmeans
+./gradlew clean jar
+cd ..
+rm -r Intermediate_Results/k${i}_[0-9]*
+
 for i in 2 4 8 12 16 24 32 48 64; do
 	java -jar kmeans/build/libs/Kmeans.jar --inputPath Intermediate_Results/train_embeddings.csv \
-	--centroidPath Intermediate_Results/train_embeddings.csv -k $i -n 25 \
+	--centroidPath Intermediate_Results/centroids_random.csv -k $i -n 25 \
 	--outputScheme Intermediate_Results/k${i}_ -skip 1
 	mv Intermediate_Results/k${i}_25/part-r-00000 Intermediate_Results/k${i}_dists
-	rm -r Intermediate_Results/k${i}_[0-9]
-	rm -r Intermediate_Results/k${i}_[0-9][0-9]
+	rm -r Intermediate_Results/k${i}_[0-9]*
 done
 
