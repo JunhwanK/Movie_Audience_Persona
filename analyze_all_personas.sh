dd8a51eb2de312548803e61faf3fda7e6ed0f3c5
Junhwan Kim <junhwank@umich.edu>
2020-04-28 01:46:46 -0400

    final results

--- analyze_all_personas.sh
+++ analyze_all_personas.sh
@@ -4,7 +4,7 @@ i=0
 NUM=$1
 while [ $i -lt $NUM ]; do
 	python3 analyze_persona.py --persona persona_ratings/persona_${i}.csv \
-		--movie_names movies.csv --output_path persona_movie_selections/persona_${i}_movies.csv \
+		--movie_names movies.csv --output_path persona_recommendations/persona_${i}_movies.csv \
 		--threshold 3.999
 	echo "completed $i"
 	i=$(expr $i + 1)
